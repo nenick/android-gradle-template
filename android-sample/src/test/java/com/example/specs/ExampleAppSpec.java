9438b47543c2e13d67b72dba51d1de8a9d6aa6a6
nkuechler <nico.kuechler@epost-dev.de>
2014-04-27 12:46:45 +0200

    remove the not necessary test-plugin
    it was obsolete since robolectric has it's own module

--- android-sample/src/test/java/com/example/specs/ExampleAppSpec.java
+++ android-sample/src/test/java/com/example/specs/ExampleAppSpec.java
@@ -2,17 +2,17 @@ package com.example.specs;
 
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.example.activity.MainActivity;
+import com.example.activity.MainActivity_;
 import com.example.test.support.EspressoSpec;
 import com.example.test.support.pages.StartPage;
 
 @LargeTest
-public class ExampleAppSpec extends EspressoSpec<MainActivity> {
+public class ExampleAppSpec extends EspressoSpec<MainActivity_> {
 
     StartPage startPage = new StartPage();
 
     public ExampleAppSpec() {
-        super(MainActivity.class);
+        super(MainActivity_.class);
     }
 
     public void testShouldShowText() {
