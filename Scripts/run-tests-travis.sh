949cb3f646f3f5f76136eb5965e9c57df0fa96e5
nkuechler <nico.kuechler@epost-dev.de>
2014-05-01 17:28:04 +0200

    try coveralls report without copy

--- Scripts/run-tests-travis.sh
+++ Scripts/run-tests-travis.sh
@@ -5,11 +5,11 @@
 # provide a source files for coveralls-gradle-plugin
 # necessary to report code coverage to coveralls
 
-mkdir -p UnitTestsRobolectric/src/main/java
-cp -r AndroidSample/src/main/java/* UnitTestsRobolectric/src/main/java
-cp -r AndroidSample/src/gen/* UnitTestsRobolectric/src/main/java
-cp -r AndroidSample/build/source/apt/debug/* UnitTestsRobolectric/src/main/java
-cp -r AndroidSample/build/source/buildConfig/debug/* UnitTestsRobolectric/src/main/java
-cp -r AndroidSample/build/source/r/debug/* UnitTestsRobolectric/src/main/java
+#mkdir -p UnitTestsRobolectric/src/main/java
+#cp -r AndroidSample/src/main/java/* UnitTestsRobolectric/src/main/java
+#cp -r AndroidSample/src/gen/* UnitTestsRobolectric/src/main/java
+#cp -r AndroidSample/build/source/apt/debug/* UnitTestsRobolectric/src/main/java
+#cp -r AndroidSample/build/source/buildConfig/debug/* UnitTestsRobolectric/src/main/java
+#cp -r AndroidSample/build/source/r/debug/* UnitTestsRobolectric/src/main/java
 
 ./gradlew :UnitTestsRobolectric:coveralls
