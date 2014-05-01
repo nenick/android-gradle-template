35dca7e1d9f1e6d942459d7043501cd3ffc73c48
nkuechler <nico.kuechler@epost-dev.de>
2014-05-01 15:04:47 +0200

    check if coverage file exist on travis

--- Scripts/run-tests-travis.sh
+++ Scripts/run-tests-travis.sh
@@ -4,4 +4,10 @@
           :ComponentTestsRobolectric:testDebug \
           :AndroidSample:connectedAndroidTest \
           :UnitTestsRobolectric:jacocoTestReport
+
+pwd
+ls UnitTestsRobolectric/build/reports/
+ls UnitTestsRobolectric/build/reports/jacoco/
+ls UnitTestsRobolectric/build/reports/jacoco/test/
+
 ./gradlew :UnitTestsRobolectric:coveralls -d
