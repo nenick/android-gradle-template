333b6944f62052ee643512a95bc70822e3fd9f56
nkuechler <nico.kuechler@epost-dev.de>
2014-05-02 15:37:29 +0200

    sometimes tests failed on emulator, following approach should avoid it

--- AndroidSample/src/test/java/com/example/test/support/EspressoSpec.java
+++ AndroidSample/src/test/java/com/example/test/support/EspressoSpec.java
@@ -2,6 +2,7 @@ package com.example.test.support;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.WindowManager;
 
 public abstract class EspressoSpec <T extends Activity> extends ActivityInstrumentationTestCase2<T> {
 
@@ -12,6 +13,16 @@ public abstract class EspressoSpec <T extends Activity> extends ActivityInstrume
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        getActivity(); // Espresso will not launch our activity for us, we must launch it via getActivity().
+
+        // sometimes tests failed on emulator, following approach should avoid it
+        // http://stackoverflow.com/questions/22737476/false-positives-junit-framework-assertionfailederror-edittext-is-not-found
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+            }
+        });
+
+        // Espresso will not launch our activity for us, we must launch it via getActivity().
     }
 }
