026f909041fdda03d9b43c021aae5d0a73b17a78
nkuechler <nico.kuechler@epost-dev.de>
2014-05-03 11:14:29 +0200

    improve plugin build script

--- Scripts/install-custom-gradle-test-plugin.sh
+++ Scripts/install-custom-gradle-test-plugin.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-$(
-    mkdir temp
-    cd temp
-    git clone https://github.com/nenick/gradle-android-test-plugin.git
-    cd gradle-android-test-plugin
+mkdir temp
+cd temp
+
+  git clone https://github.com/nenick/gradle-android-test-plugin.git
+  cd gradle-android-test-plugin
+
     ./gradlew :gradle-android-test-plugin:install
-)
+
+  cd ..
+cd ..
