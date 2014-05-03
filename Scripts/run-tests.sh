38fc9237e499ed7d10eaa32f299e7b11f647a505
nkuechler <nico.kuechler@epost-dev.de>
2014-05-03 13:45:31 +0200

    clean up dashboard content from some reports, which are not necessary

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -3,6 +3,7 @@
 ./gradlew :UnitTestsRobolectric:testDebug \
           :UnitTestsRobolectric:jacocoTestReport \
           :ComponentTestsRobolectric:testDebug \
+          :ComponentTestsRobolectric:jacocoTestReport \
           :AndroidSample:connectedCheck
 
 echo "test reports: $(pwd)/build/index.html"
