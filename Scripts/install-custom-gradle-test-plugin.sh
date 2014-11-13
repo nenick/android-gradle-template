f13c2ed22b007214af643a5fbab31fc1cea457b4
Nico Küchler <nico_kuechler@web.de>
2014-11-13 12:23:23 +0100

    Update install-custom-gradle-test-plugin.sh

--- Scripts/install-custom-gradle-test-plugin.sh
+++ Scripts/install-custom-gradle-test-plugin.sh
@@ -3,8 +3,10 @@
 mkdir temp
 cd temp
 
-  git clone https://github.com/nenick/gradle-android-test-plugin.git
+  git clone https://github.com/novoda/gradle-android-test-plugin.git
   cd gradle-android-test-plugin
+  
+    git checkout 6fcc8c8c2afc842a9e85303c62348f0bf5323971
 
     echo "rootProject.name = 'gradle-android-test-plugin-parent'" > settings.gradle
     echo "include ':gradle-android-test-plugin'" >> settings.gradle
