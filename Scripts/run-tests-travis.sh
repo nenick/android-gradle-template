eb35a66c13c7721629b6cbb94c728f1989548ae0
Nico Küchler <nico.kuechler@epost-dev.de>
2014-11-15 13:56:04 +0100

    fix coverage

--- Scripts/run-tests-travis.sh
+++ Scripts/run-tests-travis.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#Scripts/install-custom-gradle-test-plugin.sh
-
 ./gradlew :build \
           :App:test \
-          :App:connectedAndroidTest \
+          :App:connectedAndroidTestIntegrationDebug \
           :App:jacocoReport \
           :App:coveralls \
           -PtravisCi -PdisablePreDex
