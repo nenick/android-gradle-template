634e52f448f0d2e8c404f231a401b0737eb166f8
Nico Küchler <nico.kuechler@epost-dev.de>
2015-08-19 06:54:04 +0200

    clean current project

--- App/src/main/java/com/example/ExampleApplication.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-import android.app.Application;
-
-public class ExampleApplication extends Application {
-}
--- App/src/test/java/com/example/TestExampleApplication.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public class TestExampleApplication extends ExampleApplication {
-
-}
