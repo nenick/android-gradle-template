6eb54fbd925623d8f6e124204592e8fc5f7dd3c1
Nico Küchler <nico.kuechler@epost-dev.de>
2015-08-29 09:43:27 +0200

    check device state

--- scripts/src/main/resources/wait-for-boot.sh
+++ scripts/src/main/resources/wait-for-boot.sh
@@ -2,4 +2,5 @@ echo "start"
 echo "BootCompSys: `adb shell getprop sys.boot_completed`"
 echo "BootCompDev: `adb shell getprop dev.bootcomplete`"
 echo "BootAnim: `adb shell getprop init.svc.bootanim`"
+adb shell input keyevent 82
 echo "ende"
