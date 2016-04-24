d82e70ebee5fff7363e2f77ef81e790a5bcc5970
Nico Küchler <nico.kuechler@epost-dev.de>
2016-04-24 08:39:10 +0200

    fix instrumentation tests

--- apptests/src/main/java/com/example/app/ExampleTest.java
+++ apptests/src/main/java/com/example/app/ExampleTest.java
@@ -2,7 +2,9 @@ package com.example.app;
 
 import org.junit.Test;
 
-public class ExampleTest extends EspExampleTestCase {
+import de.nenick.espressomacchiato.testbase.EspressoTestCase;
+
+public class ExampleTest extends EspressoTestCase<MainActivity_> {
 
     @Test
     public void testSomething() {
