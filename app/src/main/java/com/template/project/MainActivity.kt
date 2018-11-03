e6cb2f1852b81702f51e123a67eb0cf6cdc51059
nenick <kuchler.nico@gmail.com>
2018-11-03 18:21:35 +0100

    input observer through AndroidAnnotations

--- app/src/main/java/com/template/project/MainActivity.kt
+++ app/src/main/java/com/template/project/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.template.project
 
 import android.annotation.SuppressLint
-import android.text.Editable
-import android.text.TextWatcher
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.template.project._base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.androidannotations.annotations.AfterViews
-import org.androidannotations.annotations.Bean
 import org.androidannotations.annotations.EActivity
+import org.androidannotations.annotations.TextChange
 
 @SuppressLint("Registered")
 @EActivity(R.layout.activity_main)
@@ -16,22 +15,15 @@ class MainActivity : BaseActivity() {
 
     private val model: MainViewModel by provider(MainViewModel_::class.java)
 
-    @Bean
-    protected lateinit var anything: AnyUsefulDependency
-
     @AfterViews
-    protected fun setup() {
+    fun connectViewModel() {
         model.observeTextInput(this, Observer { text ->
             textView.text = text
         })
+    }
 
-        textInput.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(text: Editable) {
-                model.updateTextInput(text.toString())
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-        })
+    @TextChange(R.id.textInput)
+    fun onInputChanged(tv: TextView, text: CharSequence) {
+        model.updateTextInput(text.toString())
     }
 }
--- app/src/main/java/com/template/project/MainViewModel.kt
+++ app/src/main/java/com/template/project/MainViewModel.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import org.androidannotations.annotations.Bean
 import org.androidannotations.annotations.EBean
 
 @EBean
 class MainViewModel : ViewModel() {
 
     private val lastTextInput = MutableLiveData<String>()
+
+    @Bean
+    protected lateinit var anything: AnyUsefulDependency
 
     fun observeTextInput(owner: LifecycleOwner, observer: Observer<in String>) {
         lastTextInput.observe(owner, observer)
