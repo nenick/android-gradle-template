44c80af8207f4711ebc9d35d4b2117122f164358
nenick <kuchler.nico@gmail.com>
2018-11-04 18:48:26 +0100

    base activity not necessary for single activity architecture

--- app/src/main/java/com/template/project/MainActivity.kt
+++ app/src/main/java/com/template/project/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.template.project
 
 import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import com.template.project.tools.BaseActivity
 import org.androidannotations.annotations.EActivity
 
 @SuppressLint("Registered")
 @EActivity(R.layout.activity_main)
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 }
