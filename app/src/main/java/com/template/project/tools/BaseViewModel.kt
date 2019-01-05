f44e99e232abd98333ea4a14a53584a8d4e4c3ea
nenick <kuchler.nico@gmail.com>
2019-01-05 23:18:19 +0100

    abstract is enough, no need for KotlinOpen

--- app/src/main/java/com/template/project/tools/BaseViewModel.kt
+++ app/src/main/java/com/template/project/tools/BaseViewModel.kt
@@ -4,5 +4,17 @@ import androidx.lifecycle.ViewModel
 import org.androidannotations.api.KotlinOpen
 import org.koin.standalone.KoinComponent
 
-@KotlinOpen
-class BaseViewModel : ViewModel(), KoinComponent
+/**
+ * The ViewModel encapsulates presentation logic and state.
+ *
+ * Presentation logic is defined here as the application logic that is concerned with the application’s
+ * use-cases (or user stories, user tasks, workflow, etc) and defines the logical behavior and structure
+ * of the application. The ViewModel is NOT a control’s code behind. Presentation logic should have no idea
+ * what its visual representation will be like and should be independent of any specific user interface
+ * implementation. To maximize re-use opportunities, the ViewModel should have no reference to any specific
+ * UI classes, elements, controls or behavior (like animations). It should not derive from any UI base class
+ * (ideally it should be a POCO). It should (of course!) be unit testable and be able to function without any UI at all.
+ *
+ * @see: https://blogs.msdn.microsoft.com/dphill/2009/01/31/the-viewmodel-pattern/
+ */
+abstract class BaseViewModel : ViewModel(), KoinComponent
