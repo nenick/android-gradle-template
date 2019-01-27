87b5c535648d67e7aa94e6b3dddced2020d089bf
nenick <kuchler.nico@gmail.com>
2019-01-27 18:57:32 +0100

    fix unit tests with receiver channel

--- app/src/test/java/com/template/project/test/tools/ViewModelTestDefaults.kt
+++ app/src/test/java/com/template/project/test/tools/ViewModelTestDefaults.kt
@@ -3,14 +3,17 @@ package com.template.project.test.tools
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.template.project.tools.ProjectDispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.scheduling.ExperimentalCoroutineDispatcher
 import org.junit.After
 import org.junit.Rule
+import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext
 import kotlin.coroutines.CoroutineContext
 
 @InternalCoroutinesApi
-abstract class RepositoryTestDefaults {
+abstract class ViewModelTestDefaults: KoinComponent {
 
     @get:Rule
     var rule = InstantTaskExecutorRule()
@@ -26,4 +29,3 @@ abstract class RepositoryTestDefaults {
         StandAloneContext.stopKoin()
     }
 }
-
--- app/src/test/java/com/template/project/views/contentsample/ContentSampleViewModelTest.kt
+++ app/src/test/java/com/template/project/views/contentsample/ContentSampleViewModelTest.kt
@@ -1,84 +1,64 @@
 package com.template.project.views.contentsample
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.MutableLiveData
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import com.template.project.data.local.entities.Todo
 import com.template.project.model.repositories.TodoRepository
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.channels.ChannelIterator
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.test.TestCoroutineContext
-import org.junit.After
-import org.junit.Rule
+import com.template.project.test.tools.ViewModelTestDefaults
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.produce
+import org.junit.Before
 import org.junit.Test
-import org.junit.rules.TestRule
 import org.koin.dsl.module.module
-import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext
-import org.koin.standalone.inject
-import org.mockito.Mockito
-import kotlin.coroutines.CoroutineContext
+import org.mockito.Mockito.verify
 
 @InternalCoroutinesApi
-class ContentSampleViewModelTest : KoinComponent {
-
-    @get:Rule
-    var rule: TestRule = InstantTaskExecutorRule()
-
-    private val lifecycleOwner = {
-        LifecycleRegistry(Mockito.mock(LifecycleOwner::class.java)).apply {
-            handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        }
-    }
+class ContentSampleViewModelTest : ViewModelTestDefaults() {
 
     private val repository = mock<TodoRepository>()
     private val repositoryTodoResult = MutableLiveData<List<Todo>>()
 
-    private val model by lazy { ContentSampleViewModel() }
+    private lateinit var producer: ProducerScope<List<Todo>>
 
-    private val repRes = mock<ReceiveChannel<List<Todo>>>()
-
-    init {
-        //whenever(repository.loadTodoListResource()).thenReturn(repRes)
-        val mockIte = mock<ChannelIterator<List<Todo>>> {
-            //onBlocking { hasNext() } doReturn false
-        }
-        whenever(repRes.iterator()).thenReturn(mockIte)
-
-        StandAloneContext.startKoin(listOf(module {
-            single { TestCoroutineContext() }
-            single { get<TestCoroutineContext>() as CoroutineContext }
-            factory { repository }
-        }))
-
-        model // just trigger lazy initialisation
+    private val repRes = CoroutineScope(coroutineDispatcher.io).produce<List<Todo>> {
+        producer = this
+        // Loop to keep the producer channel alive, or no more data will be received.
+        while (isActive) delay(1000)
+    }.apply {
+        runBlocking { whenever(repository.observeTodoList()).thenReturn(this@apply) }
     }
 
-    val coroutineContext by inject<TestCoroutineContext>()
+    init {
+        StandAloneContext.startKoin(listOf(module {
+            single { coroutineDispatcher }
+            factory { repository }
+        }))
+    }
+
+    private val model by lazy { ContentSampleViewModel() }
+
+    @Before
+    fun setup() {
+        model // just initialise it (now we have the test rule executor scope available and it is save)
+        repositoryTodoResult.observeForever { runBlocking { producer.send(it) } }
+    }
 
     @Test
     fun `load todo list on initialisation`() {
-        model.todoList
-        coroutineContext.triggerActions()
-
-        val ex = coroutineContext.exceptions
-        //verify(repository).loadTodoListResource()
+        runBlocking { verify(repository).observeTodoList() }
+        runBlocking { verify(repository).fetchTodoList() }
     }
 
     @Test
     fun `provide todo list result`() {
         repositoryTodoResult.value = emptyList()
-        assertThat(model.todoList.value).isNotNull()
-    }
+        assertThat(model.todoList.value).isEmpty()
 
-    @After
-    fun clean() {
-        StandAloneContext.stopKoin()
+        repositoryTodoResult.value = listOf(Todo(1, 1, "TestTodo", true))
+        assertThat(model.todoList.value).isNotEmpty()
     }
 }
