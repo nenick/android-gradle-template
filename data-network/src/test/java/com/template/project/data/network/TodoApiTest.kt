294b92ad12c219a160a54b0a2e9f8ce59f6d8eaf
Nico Küchler <kuchler.nico@gmail.com>
2019-02-03 08:48:21 +0100

    test for api success state

--- data-network/src/test/java/com/template/project/data/network/TodoApiTest.kt
+++ data-network/src/test/java/com/template/project/data/network/TodoApiTest.kt
@@ -17,18 +17,21 @@ class TodoApiTest : ApiTest() {
     @Test
     fun `fetch ToDo list`() {
         val result = runBlocking { api.allTodo() }
+        assertThat(result.isSuccessful).isTrue()
         assertThat(result.body).hasSize(200)
     }
 
     @Test
     fun `fetch ToDo`() {
         val result = runBlocking { api.todo(testTodo) }
+        assertThat(result.isSuccessful).isTrue()
         assertThat(result.body).isEqualTo(TodoJson(1, 1, "delectus aut autem", false))
     }
 
     @Test
     fun `fetch ToDo - on failure`() {
         val result = runBlocking { api.todo(testTodoNotExist) }
+        assertThat(result.isSuccessful).isFalse()
         assertThat(result.errorMessage).isEqualTo("Not Found")
     }
 }
