93d554d5a59248409cbb6a174bb486b4943f492d
nenick <kuchler.nico@gmail.com>
2020-06-23 15:58:06 +0200

    coverage shows all files in report

--- pipelines/templates/collect-sources.sh
+++ pipelines/templates/collect-sources.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-cp buildSrc/src/main/kotlin build/coverage/sources
-cp app/src/main/java build/coverage/sources
-cp core-data-network/src/main/java build/coverage/sources
+mkdir -p build/coverage/sources
+cp -r buildSrc/src/main/kotlin/* build/coverage/sources
+cp -r app/src/main/java/* build/coverage/sources
+cp -r core-data-network/src/main/java/* build/coverage/sources
