de6b28965587a81dd1d719d096d69882bdfaed01
nenick <kuchler.nico@gmail.com>
2020-06-25 21:52:08 +0200

    optimize emulator startup steps

--- pipelines/templates/android-emulator-install.sh
+++ pipelines/templates/android-emulator-install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 # Install AVD files
-(echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;google_apis;x86' && touch installed) &
+(
+    echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;google_apis;x86'
+
+    # Create emulator
+    echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android-v29 -k 'system-images;android-29;google_apis;x86' --force
+
+    touch installed
+) &
--- pipelines/templates/android-emulator-start.sh
+++ pipelines/templates/android-emulator-start.sh
@@ -2,12 +2,11 @@
 
 while [[ ! -f installed ]]; do sleep 1; done;
 
-# Create emulator
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android-v29 -k 'system-images;android-29;google_apis;x86' --force
+(
+    $ANDROID_HOME/emulator/emulator -list-avds
 
-$ANDROID_HOME/emulator/emulator -list-avds
+    echo "Starting emulator"
 
-echo "Starting emulator"
-
-# Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd android-v29 -no-snapshot > /dev/null 2>&1 &
+    # Start emulator in background
+    nohup $ANDROID_HOME/emulator/emulator -avd android-v29 -no-snapshot > /dev/null 2>&1
+) &
