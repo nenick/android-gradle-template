16577e4137979a3516e413dc90d3bb95913a0721
nenick <kuchler.nico@gmail.com>
2020-08-14 20:43:46 +0200

    check module list as executable script

---
+++ pipelines/tasks/check-module-list.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+MODULES_LISTED=$(echo $1 | sed $'s/ /\\\n/g')
+
+MODULES_EXISTS=$(cat settings.gradle | sed s/"include ':"//g | sed s/\:/\\//g | sed s/\'//g)
+MODULES_EXISTS=$(echo -e "buildSrc\\n$MODULES_EXISTS")
+
+echo ""
+echo "== MODULES_EXISTS =="
+echo "$MODULES_EXISTS"
+echo ""
+echo "== MODULES_LISTED =="
+echo "$MODULES_LISTED"
+echo ""
+
+if [ "$MODULES_EXISTS" != "$MODULES_LISTED" ]; then
+  >&2 echo "Error: listed modules does not match settings.gradle"
+  exit 1
+fi
