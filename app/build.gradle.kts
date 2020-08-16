3400d355f0db5731d34908d2e055d0168b434fc3
nenick <kuchler.nico@gmail.com>
2020-08-16 23:30:05 +0200

    more documentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,38 +14,69 @@ android {
         minSdkVersion(21) // The minSdk could be below 21 but then we have to handle multidex by self.
         targetSdkVersion(29)
 
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 1 // TODO use the timestamp / 10 algorithm which avoids conflicts for the next few hundreds years.
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            // Default generated proguard configuration, see also https://developer.android.com/studio/build/shrink-code
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("debug") {
+            // TODO move this to onDeviceServer buildType because we use only that for automatic testing.
+            // We have it still in debug to support coverage report checks, which do report when coverage is not enabled.
             isTestCoverageEnabled = true
         }
+
+        //================================================================================ #onDeviceServer
+        // Support for providing an on device mock server for stable demos and testing.
+        //
+        // Change the testBuildType if you want to target a different server for automated
+        // testing e.g. a local started WireMock instance.
+        //
+        // Requirements: We want to support on device mock server without effecting to
+        // many variants because current it needs a lot of customizations which reduce
+        // realism (e.g. include legacy http client). It should be possible to use all
+        // app variants with the on device server for demo and testing. Also it shouldn't
+        // introduce to much project setup complexity. That's why we prefer buildType
+        // instead of product flavor.
+        //
+        // See also
+        // * Request to support multiple buildTypes https://issuetracker.google.com/issues/154260222
+        // * Missing hint why test classes aren't resolvable https://issuetracker.google.com/issues/148451831
+        //
         create("onDeviceServer") {
+            // Make sure that new developers will have less issues when they want to run
+            // the androidTest the first time after initial cloning.
+            isDefault = true
+            // Basically it should have the same setup as the debug variant.
             initWith(getByName("debug"))
             matchingFallbacks = listOf("debug")
         }
     }
 
+    // Search for #onDeviceServer
+    testBuildType = "onDeviceServer"
+
     lintOptions {
+        // We handle each reported lint issue as an error which sometimes detects real important issues.
         isWarningsAsErrors = true
         isAbortOnError = false
     }
 
     packagingOptions {
-        // avoids e.g. More than one file was found with OS independent path "META-INF/ktor-client-json.kotlin_module".
+        // Avoids issues like: More than one file was found with OS independent path "META-INF/ktor-client-json.kotlin_module".
         exclude("META-INF/*.kotlin_module")
     }
 }
 
 dependencies {
+    // With Kotlin DSL we have to "access" first the dependency configuration  target for different variants.
+    // The variable naming will be checked by the compiler that this variant does really exists.
     val onDeviceServerImplementation by configurations
 
     implementation(project(":core:todo-api"))
@@ -64,6 +95,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit")
     androidTestImplementation("androidx.test.espresso:espresso-core")
+    androidTestImplementation("androidx.test:rules:1.2.0")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
