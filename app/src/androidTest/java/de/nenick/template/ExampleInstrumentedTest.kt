cc061cfa9b1731acc197645c9b0e860a96e8f36e
nenick <kuchler.nico@gmail.com>
2020-08-16 23:30:25 +0200

    sample test for on device server

--- app/src/androidTest/java/de/nenick/template/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/nenick/template/ExampleInstrumentedTest.kt
@@ -1,8 +1,18 @@
 package de.nenick.template
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,10 +23,21 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("de.nenick.template", appContext.packageName)
+    }
+
+    @Test
+    fun sample() {
+        onView(withId(R.id.textView)).check(matches(withText("Hello World!")))
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("My Task 4")))
     }
 }
